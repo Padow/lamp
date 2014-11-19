92d82b667e33649619546dc3e095c13b8cd8a009
Padow <patrick.douchement@gmail.com>
2014-11-19 22:05:47 +0100

    add loaded modules

--- index.php
+++ index.php
@@ -55,6 +55,7 @@
  	$php_version = phpversion();
  	
 	$loaded_extensions = get_loaded_extensions();
+	$loaded_modules = apache_get_modules();
 	function find_SQL_Version() {
 	   $output = shell_exec('mysql -V');
 	   preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
@@ -65,49 +66,70 @@
 
 ?>
 <div class="container">
-<div class="col">
+<div class="col-md-12">
 	<?php if(apache_version()){?>
 		<img src="lamp_files/images/logo.png" class="img-responsive" alt="Responsive image">
 	<?php }else{ ?>
 		<img src="lamp_files/images/lnamp.png" class="img-responsive" alt="Responsive image">
 	<?php } ?>
 	<hr>
-	<div class="col">
+	<div class="col-md-12">
 		<h3>Config server</h3>
 		<?php if(apache_version()){?>
 		<ul><strong>Apache version : </strong><?= $apache_version ?></ul><?php } ?>
 		<ul><strong>PHP version : </strong><?= $php_version ?></ul>
 		<ul><strong>MySQL version : </strong><?= $SQL_version ?></ul>
-		<ul><strong>Loaded extensions : </strong>
-
-		<div class="table-responsive">
-		<div class="col">
-		<table class="table table-condensed">
-		<tr>
-		<?php 
-		$cpt = 1;
-		foreach ($loaded_extensions as $extension){
-			if($cpt > 8){
-				echo '</tr><tr>';
-				$cpt = 1;
-			}
-			echo '<td class="noborder col-md-1"><span class="glyphicon glyphicon-cog"></span> '.$extension.'</td>'; 
-			$cpt++;
+		<div class="tt">
+		 <?php 
+		foreach ($loaded_modules as $module){
+			echo '<div class="module"><span class="glyphicon glyphicon-cog"></span> '.$module.'</div>'; 
 		}
-		?>
-		</tr>
-
-		</table>
+		?></div>
+		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingOne">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		          Loaded extentions
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+		      <div class="panel-body">
+		        <?php 
+				foreach ($loaded_extensions as $extension){
+					echo '<div class="extention"><span class="glyphicon glyphicon-cog"></span> '.$extension.'</div>'; 
+				}
+				?>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="panel panel-default">
+		    <div class="panel-heading" role="tab" id="headingTwo">
+		      <h4 class="panel-title">
+		        <a class="collapsed" id="target" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+		          Loaded modules
+		        </a>
+		      </h4>
+		    </div>
+		    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+		      <div class="panel-body">
+		        <?php 
+				foreach ($loaded_modules as $module){
+					echo '<div class="module"><span class="glyphicon glyphicon-cog"></span> '.$module.'</div>'; 
+				}
+				?>
+		      </div>
+		    </div>
+		  </div>
 		</div>
-		</div>
-		</ul>	
 	</div>
-	<div class="col">
+	<div class="col-md-12">
 		<h3>Tools</h3>
 		<ul><a href="<?= DBCLIENTLOC ?>" class="btn-link"><span class="glyphicon glyphicon-wrench"></span> <?= DBCLIENTNAME ?></a></ul>
 		<ul><a href="lamp_files/phpinfo.php" class="btn-link"><span class="glyphicon glyphicon-wrench"></span> phpinfo()</a></ul>	
 	</div>
-	<div class="col">
+	<div class="col-md-12">
 		<h3>Projects</h3>		
 		<?php 
 			$projectsListIgnore = array ('.','..','lamp_files', DBCLIENTLOC);
@@ -141,7 +163,32 @@
     </div>
   </div>
   </div>
+<script type="text/javascript" src="lamp_files/jquery.js"></script>
 <script type="text/javascript" src="lamp_files/css/bootstrap/js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+var min_width = 1;
+$('.extention').each(function(){
+        var this_width = $(this).width();
+        if (this_width > min_width) min_width = this_width;
+});
+$('.extention').width(min_width+10);
+
+
+
+$( "#target" ).click(function() {
+setTimeout(
+  function() 
+  {
+  	var min_width_module = 1;
+$('.module').each(function(){
+        var this_width = $(this).width();
+        if (this_width > min_width_module) min_width_module = this_width;
+});
+$('.module').width(min_width_module+10);
+  }, 10);
+});
+</script>
 </div>
 </body>
 </html>
